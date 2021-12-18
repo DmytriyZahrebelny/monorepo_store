f555a2f42eac1e19808c8392f59e7a448958f756
Dima <zahrebelny@gmail.com>
2021-12-18 22:38:05 +0200

    updated react-router to version 6

--- apps/store/src/authorization/sign-up/use-sign-up.ts
+++ apps/store/src/authorization/sign-up/use-sign-up.ts
@@ -1,21 +1,21 @@
 import { useMutation } from '@apollo/client';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import RegisterMutation from '../../graphql/Register.mutation.gql';
 import { RegisterMutationType, RegistrMutationVariables, RegisterInput } from '../../types';
 import { ROUTES } from '../../app.constants';
 
 export const useSignUp = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const [register] = useMutation<RegisterMutationType, RegistrMutationVariables>(RegisterMutation);
 
   const onSubmit = (data: RegisterInput) => {
     const { email, password, name } = data;
     register({ variables: { input: { email, password, name } } });
 
-    history.push(ROUTES.singIn);
+    navigate(ROUTES.singIn);
   };
 
   const schema = yup.object().shape({
